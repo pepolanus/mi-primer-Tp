00ddef8b3c9fb7bfee97be0d5166470dcd2638b2
eugesaguier <eugesaguier@udesa.edu.ar>
2023-11-10 10:38:48 -0300

    javahome

--- js/home.js
+++ js/home.js
@@ -1,12 +1,62 @@
-fetch('https://api.themoviedb.org/3/account/{account_id}/watchlist/movies?api_key=2824d7c18fccbca72f06f7f29e7ebcd8')
+let peliculasFavoritas = document.querySelector('.Peliculasfavoritas')
+
+fetch('https://api.themoviedb.org/3/discover/movie?api_key=2824d7c18fccbca72f06f7f29e7ebcd8')
 .then(function(response){
     return response.json()
 })
 .then(function(data){
-    console.log(data)
-    let PFav = data
+    for (let i = 0; i < 5; i++){
+        peliculasFavoritas.innerHTML += `
+        <article>
+            <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" />
+            <h2>${data.results[i].title}</h2>
+        </article>`
+    }
 })
 .catch(function(error){
-    console.log(error)
+    alert('Error! Intente mas tarde')
 })
 
+
+let seriesfav = document.querySelector('.seriesfav')
+
+
+fetch('https://api.themoviedb.org/3/discover/tv?api_key=2824d7c18fccbca72f06f7f29e7ebcd8')
+.then(function(response){
+    console.log('entra al primer then')
+    return response.json()
+})
+.then(function(data){
+    console.log(data.results);
+    for (let i = 0; i < 5; i++, i<5){
+        seriesfav.innerHTML += `
+        <article>
+            <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" />
+            <h2>${data.results[i].name}</h2>
+        </article>`
+    }
+})
+.catch(function(error){
+    alert('Error! Intente mas tarde')
+})
+
+
+
+let masvis = document.querySelector('.masvis')
+
+fetch('https://api.themoviedb.org/3/discover/movie?api_key=2824d7c18fccbca72f06f7f29e7ebcd8')
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    for (let i = 0; i < 5; i++, i<5){
+        masvis.innerHTML += `
+        <article>
+            <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" />
+            <h2>${data.results[i].title}</h2>
+        </article>`
+    }
+})
+.catch(function(error){
+    alert('Error! Intente mas tarde')
+})
