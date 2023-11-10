48016d427ad46c45c2be67c5f9c430a1fb8d4dd8
montielchechi <emontiel@udesa.edu.ar>
2023-11-10 11:14:56 -0300

    cambiosvarios

--- js/home.js
+++ js/home.js
@@ -44,17 +44,17 @@ fetch('https://api.themoviedb.org/3/discover/tv?api_key=2824d7c18fccbca72f06f7f2
 
 let masvis = document.querySelector('.masvis')
 
-fetch('https://api.themoviedb.org/3/discover/movie?api_key=2824d7c18fccbca72f06f7f29e7ebcd8')
+fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=2824d7c18fccbca72f06f7f29e7ebcd8')
 .then(function(response){
     return response.json()
 })
 .then(function(data){
     for (let i = 0; i < 5; i++, i<5){
         masvis.innerHTML += `
-        <article>
+        <a href="./detail-pelis.html?id=${data.results[i].id}"><article>
             <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" />
             <h2>${data.results[i].title}</h2>
-        </article>`
+        </article></a>`
     }
 })
 .catch(function(error){
