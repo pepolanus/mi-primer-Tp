041c979d8d0d5c7b29aa4e17a4202dac311e8807
montielchechi <emontiel@udesa.edu.ar>
2023-11-10 12:07:57 -0300

    seriesdetail

--- js/detail-series.js
+++ js/detail-series.js
@@ -3,7 +3,7 @@ let objBusqueda = new URLSearchParams(query)
 let id= objBusqueda.get('id')
 
 let detalles = document.querySelector('.detalles')
-fetch(`https://api.themoviedb.org/3/discover/tv/${id}?api_key=2824d7c18fccbca72f06f7f29e7ebcd8`)
+fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=2824d7c18fccbca72f06f7f29e7ebcd8`)
 .then(function(response){
     return response.json()
 })
