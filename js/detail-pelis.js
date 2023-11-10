7cb8ad77677f2ee4e08bdea2d42cd03ce0974a5c
eugesaguier <eugesaguier@udesa.edu.ar>
2023-11-10 12:08:54 -0300

    javacambiosx

--- js/detail-pelis.js
+++ js/detail-pelis.js
@@ -13,6 +13,11 @@ fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=2824d7c18fccbca72f06f7f2
             <article>
                 <img src="https://image.tmdb.org/t/p/w500/${data.poster_path}" />
                 <h2>${data.title}</h2>
+                <h3>${data.overview} </h3>
+                <h3>${data.runtime} </h3>
+                <h3>${data.release_date} </h3>
+                <h3>${data.genres[0].name} </h3>
+                <h3>${data.vote_average} </h3>
             </article>`
 
 })
