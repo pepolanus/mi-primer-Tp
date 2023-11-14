a66fb9c28e675448719e8bfe527a311d1259f600
montielchechi <emontiel@udesa.edu.ar>
2023-11-14 08:30:53 -0300

    recomendacionesterminadas

--- js/detail-series.js
+++ js/detail-series.js
@@ -24,3 +24,22 @@ fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=2824d7c18fccbca72f06f7f29e7
 .catch(function(error){
     console.log('Error! Intente mas tarde')
 })
+
+let seriesreco = document.querySelector('.seriesreco')
+
+fetch('https://api.themoviedb.org/3/tv/top_rated?api_key=2824d7c18fccbca72f06f7f29e7ebcd8')
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    for (let i = 0; i < 5; i++, i<5){
+        seriesreco.innerHTML += `
+        <a href="./detail-series.html?id=${data.results[i].id}"><article>
+            <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" />
+            <h2>${data.results[i].name}</h2>
+        </article></a>`
+    }
+})
+.catch(function(error){
+    console.log('error')
+})
